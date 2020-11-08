53e2357c2da061089d96060800a480954b875b88
Sarah Chow <sarahrch@usc.edu>
2020-11-07 16:58:54 -0800

    snake boxes

--- snake_controller.v
+++ snake_controller.v
@@ -35,7 +35,10 @@ module snake_controller(
 	always@ (*) begin
     	if(~Bright )	//force black if not inside the display area
 			rgb = 12'b0000_0000_0000;
-		else if (snake_fill) 
+		else if (snake_fill0 || snake_fill1 || snake_fill2 || snake_fill3 
+				|| snake_fill4 || snake_fill5 || snake_fill6 || snake_fill7 
+				|| snake_fill8 || snake_fill9 || snake_fill10 || snake_fill11 
+				|| snake_fill12 || snake_fill13 || snake_fill14 || snake_fill15) 
 			rgb = YELLOW; 
 		else if (food_fill)
 			rgb = 12'b1111_1111_1111;
@@ -65,11 +68,23 @@ module snake_controller(
 		*/
 	
 	//the +-15 for the positions give the dimension of the block (i.e. it will be 30x30 pixels)
-	integer j;
-	for (j = 0; j < Length; j = j + 1)
-		begin
-			assign snake_fill = vCount>=(ypos[j]-15) && vCount<=(ypos[j]+15) && hCount>=(xpos[j]-15) && hCount<=(xpos[j]+15);
-		end
+	assign snake_fill0 = vCount>=(ypos[0]-15) && vCount<=(ypos[0]+15) && hCount>=(xpos[0]-15) && hCount<=(xpos[0]+15);
+	assign snake_fill1 = vCount>=(ypos[1]-15) && vCount<=(ypos[1]+15) && hCount>=(xpos[1]-15) && hCount<=(xpos[1]+15);
+	assign snake_fill2 = vCount>=(ypos[2]-15) && vCount<=(ypos[2]+15) && hCount>=(xpos[2]-15) && hCount<=(xpos[2]+15);
+	assign snake_fill3 = vCount>=(ypos[3]-15) && vCount<=(ypos[3]+15) && hCount>=(xpos[3]-15) && hCount<=(xpos[3]+15);
+	assign snake_fill4 = vCount>=(ypos[4]-15) && vCount<=(ypos[4]+15) && hCount>=(xpos[4]-15) && hCount<=(xpos[4]+15);
+	assign snake_fill5 = vCount>=(ypos[5]-15) && vCount<=(ypos[5]+15) && hCount>=(xpos[5]-15) && hCount<=(xpos[5]+15);
+	assign snake_fill6 = vCount>=(ypos[6]-15) && vCount<=(ypos[6]+15) && hCount>=(xpos[6]-15) && hCount<=(xpos[6]+15);
+	assign snake_fill7 = vCount>=(ypos[7]-15) && vCount<=(ypos[7]+15) && hCount>=(xpos[7]-15) && hCount<=(xpos[7]+15);
+	assign snake_fill8 = vCount>=(ypos[8]-15) && vCount<=(ypos[8]+15) && hCount>=(xpos[8]-15) && hCount<=(xpos[8]+15);
+	assign snake_fill9 = vCount>=(ypos[9]-15) && vCount<=(ypos[9]+15) && hCount>=(xpos[9]-15) && hCount<=(xpos[9]+15);
+	assign snake_fill10 = vCount>=(ypos[10]-15) && vCount<=(ypos[10]+15) && hCount>=(xpos[10]-15) && hCount<=(xpos[10]+15);
+	assign snake_fill11 = vCount>=(ypos[11]-15) && vCount<=(ypos[11]+15) && hCount>=(xpos[11]-15) && hCount<=(xpos[11]+15);
+	assign snake_fill12 = vCount>=(ypos[12]-15) && vCount<=(ypos[12]+15) && hCount>=(xpos[12]-15) && hCount<=(xpos[12]+15);
+	assign snake_fill13 = vCount>=(ypos[13]-15) && vCount<=(ypos[13]+15) && hCount>=(xpos[13]-15) && hCount<=(xpos[13]+15);
+	assign snake_fill14 = vCount>=(ypos[14]-15) && vCount<=(ypos[14]+15) && hCount>=(xpos[14]-15) && hCount<=(xpos[14]+15);
+	assign snake_fill15 = vCount>=(ypos[15]-15) && vCount<=(ypos[15]+15) && hCount>=(xpos[15]-15) && hCount<=(xpos[15]+15);
+	
 	assign food_fill = vCount>=(f_ypos-15) && vCount<=(f_ypos+15) && hCount>=(f_xpos-15) && hCount<=(f_xpos+15);
 	
 	
